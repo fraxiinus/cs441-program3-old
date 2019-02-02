fd28031c221277a381c5e9d22e05bcde807a4932
Andrew Lee <andrew1421lee@gmail.com>
2019-02-02 16:24:07 -0500

    work on getting game to start

--- app/src/main/java/com/etirps/zhu/gamedemo/MainActivity.kt
+++ app/src/main/java/com/etirps/zhu/gamedemo/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.etirps.zhu.gamedemo
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.OvalShape
-import android.graphics.drawable.shapes.RectShape
-import android.graphics.drawable.shapes.Shape
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import top.defaults.drawabletoolbox.DrawableBuilder
 
@@ -42,44 +35,13 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        val x : Float = event.x
-        val y : Float = event.y
-
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                mInitialX = x
-                mInitialY = y
-            }
-
-            MotionEvent.ACTION_MOVE -> {
-                var dx : Float = x - mInitialX
-                var dy : Float = y - mInitialY
-            }
+    override fun onPause() {
+        if(!mTitleVisible) {
+            copyrightTitleText.animateAlpha(duration = mAnimationDuration, toAlpha = 1.0f, setVisibility = View.VISIBLE)
+            gameTitleButton.animateAlpha(duration = mAnimationDuration, toAlpha = 1.0f, setVisibility = View.VISIBLE)
+            gameTitleText.animateAlpha(duration = mAnimationDuration, toAlpha = 1.0f, setVisibility = View.VISIBLE)
         }
-
-        copyrightTitleText.text = "$x by $y"
-
-        val what = DrawableBuilder()
-            .oval()
-            .solidColor(Color.TRANSPARENT)
-            .strokeColor(Color.WHITE)
-            .strokeWidth(5)
-            .size(40)
-            .build()
-
-        val img = ImageView(this)
-
-        img.maxHeight = 40
-        img.maxWidth = 40
-        img.x = x - 20
-        img.y = y - 100
-        img.setImageDrawable(what)
-        img.setBackgroundColor(Color.GRAY)
-
-        main_layout.addView(img)
-
-        return true
+        super.onPause()
     }
 
     fun startButtonPress(view: View) {
@@ -88,9 +50,23 @@ class MainActivity : AppCompatActivity() {
         // Disappear title
         copyrightTitleText.animateAlpha(duration = mAnimationDuration, offset = 500, setVisibility = View.GONE)
         gameTitleButton.animateAlpha(duration = mAnimationDuration, offset = 500, setVisibility = View.GONE)
-        gameTitleText.animateAlpha(duration = mAnimationDuration, offset = 500, setVisibility = View.GONE)
+
+        gameTitleText.animate()
+            .setDuration(mAnimationDuration)
+            .alpha(0.0f)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    gameTitleText.visibility = View.GONE
+                    startGame()
+                }
+            })
+            .startDelay = 500
 
         mTitleVisible = false
+
     }
 
+    fun startGame() {
+        startActivity(Intent(this, GameActivity::class.java))
+    }
 }
